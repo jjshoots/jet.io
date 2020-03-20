19bd2e2924d0a62804df11b45e93b797cc3aa9fc
HEDWIG <taijunjet@hotmail.com>
2020-03-20 13:57:16 +0800

    update zoey page to have 2 buttons

--- clock/scripts/main.js
+++ clock/scripts/main.js
@@ -25,6 +25,42 @@ function displayTime(){
     timeSpace.innerText = timeText;
 }
 
+function timeToEat(){
+	var currentTime = new Date();
+	if(12 <= currentTime.getHours() && currentTime.getHours() <= 18 ||
+		18 <= currentTime.getHours() && currentTime.getHours() <= 21){
+		var catMessage = document.getElementById('catMessage');
+		var randNum = Math.floor(Math.random() * 4);
+		switch(randNum){
+			case 0:
+				catMessage.innerText = "Hello have you eaten?";
+				break;
+			case 1:
+				catMessage.innerText = "Jiak Beng Bo?";
+				break;
+			case 2:
+				catMessage.innerText = "Did you bon apetit-ed?";
+				break;
+			case 3:
+				catMessage.innerText = "你吃过了没？";
+				break;
+
+		}
+	}else{
+		var buttons = document.getElementById('buttonArea');
+		buttons.remove();
+	}
+}
+
+function yesClick(){
+
+}
+
+function noClick(){
+	
+}
+
 displayTime();
+timeToEat();
 
 setInterval(displayTime, 500);

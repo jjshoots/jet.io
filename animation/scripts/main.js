d468f7daf8d778f405472f5d0240b34541c74017
HEDWIG <taijunjet@hotmail.com>
2020-04-01 23:21:31 +0800

    completed raycasting v1.0

--- animation/scripts/main.js
+++ animation/scripts/main.js
@@ -1,19 +1,33 @@
 const screenWidth = window.innerWidth;
 const screenHeight = window.innerHeight;
 
-var obsLine = [
-	[0, 0, screenWidth, 0],
-	[0, screenHeight, screenWidth, screenHeight],
-	[0, 0, 0, screenHeight],
-	[screenWidth, screenHeight, screenWidth, screenHeight]
-];
-
-
-
 function setup() {
   createCanvas(screenWidth, screenHeight);
   background(255);
   obj = new sun(20, screenWidth, screenHeight);
+  objLines = new obsLine();
+
+  // z
+  objLines.parseIn([100, 100], [300, 100]);
+  objLines.parseIn([300, 100], [100, 300]);
+  objLines.parseIn([100, 300], [300, 300]);
+
+  // o
+  objLines.parseIn([350, 100], [550, 100]);
+  objLines.parseIn([550, 100], [550, 300]);
+  objLines.parseIn([550, 300], [350, 300]);
+  objLines.parseIn([350, 300], [350, 100]);
+
+  // e
+  objLines.parseIn([600, 100], [800, 100]);
+  objLines.parseIn([600, 200], [800, 200]);
+  objLines.parseIn([600, 300], [800, 300]);
+  objLines.parseIn([600, 100], [600, 300]);
+
+  // y
+  objLines.parseIn([850, 100], [950, 200]);
+  objLines.parseIn([1050, 100], [950, 200]);
+  objLines.parseIn([950, 200], [950, 300]);
 }
 
 function draw() {
@@ -21,6 +35,13 @@ function draw() {
 	obj.update();
 	obj.findC();
 	obj.interceptScreenEdges();
+	// objLines.draw();
+
+
+	for(var i = 0; i < objLines.numLines; i++){
+		obj.findIntercept(objLines.parseOut(i));
+	}
+
 	obj.cast();
 	// noLoop();
 }
--- animation/scripts/sunClass.js
+++ animation/scripts/sunClass.js
@@ -24,15 +24,17 @@ class sun{
 
 		this.angles = new Array(this.rayNumber);
 		this.gradients = new Array(this.rayNumber);
-		for(var i = 0; i < this.rayNumber; i++){
-			this.angles[i] = i * (2 * Math.PI / this.rayNumber);
+		this.angles[0] = 2 * Math.PI / this.rayNumber;
+		this.gradients[0] = tan(this.angles[0]);
+		for(var i = 1; i < this.rayNumber; i++){
+			this.angles[i] = this.angles[i-1] + this.angles[0];
 			if(this.angles[i] === 0.5 * Math.PI || this.angles[i] === 1.5 * Math.PI){
 				this.gradients[i] = Number.NaN;
 			}else{
 				this.gradients[i] = tan(this.angles[i]);
 			}
 		}
-		console.log(this.angles);
+		// console.log(this.angles);
 
 		this.C = new Array(this.rayNumber);
 	}
@@ -63,14 +65,14 @@ class sun{
 
 					if((this.angles[j] < 0.5 * Math.PI || this.angles[j] > 1.5 * Math.PI) && 1){
 						if(interceptX > this.originX){
-							this.intercepts[j][1] = interceptX;
-							this.intercepts[j][2] = interceptY;
+							this.intercepts[j][0] = interceptX;
+							this.intercepts[j][1] = interceptY;
 						}
 					}else
 					if((this.angles[j] > 0.5 * Math.PI || this.angles[j] < 1.5 * Math.PI) && 1){
 						if(interceptX < this.originX){
-							this.intercepts[j][1] = interceptX;
-							this.intercepts[j][2] = interceptY;
+							this.intercepts[j][0] = interceptX;
+							this.intercepts[j][1] = interceptY;
 						}
 					}
 				}
@@ -86,28 +88,110 @@ class sun{
 
 					if(this.angles[j] < 0.5 * Math.PI || this.angles[j] > 1.5 * Math.PI){
 						if(interceptX > this.originX){
-							if(interceptX < this.intercepts[j][1]){
-								this.intercepts[j][1] = interceptX;
-								this.intercepts[j][2] = interceptY;
+							if(interceptX < this.intercepts[j][0]){
+								this.intercepts[j][0] = interceptX;
+								this.intercepts[j][1] = interceptY;
 							}
 						}
 					}else
 					if(this.angles[j] > 0.5 * Math.PI || this.angles[j] < 1.5 * Math.PI){
 						if(interceptX < this.originX){
-							if(interceptX > this.intercepts[j][1]){
-								this.intercepts[j][1] = interceptX;
-								this.intercepts[j][2] = interceptY;
+							if(interceptX > this.intercepts[j][0]){
+								this.intercepts[j][0] = interceptX;
+								this.intercepts[j][1] = interceptY;
 							}
 						}
 					}
 				}else{
 					if(this.angles[j] < Math.PI){
-						this.intercepts[j][1] = this.originX;
-						this.intercepts[j][2] = 0;
+						this.intercepts[j][0] = this.originX;
+						this.intercepts[j][1] = this.screenHeight;
 					}else
 					if(this.angles[j] > Math.PI){
-						this.intercepts[j][1] = this.originX;
-						this.intercepts[j][2] = this.screenHeight;
+						this.intercepts[j][0] = this.originX;
+						this.intercepts[j][1] = 0;
+					}
+				}
+			}
+		}
+	}
+
+	findIntercept(lineConst){
+		for(var j = 0; j < this.rayNumber; j++){
+			if(!isNaN(this.gradients[j])){
+				if(isFinite(lineConst[0])){
+					var interceptX = (this.C[j] - lineConst[1]) / (lineConst[0] - this.gradients[j]);
+					var interceptY = this.gradients[j] * interceptX + this.C[j];
+
+					if(this.angles[j] < 0.5 * Math.PI || this.angles[j] > 1.5 * Math.PI){
+						if(interceptX > this.originX){
+							var dist = interceptX - lineConst[2]; 
+							if(0 <= dist && dist < lineConst[3] - lineConst[2]){
+								if(interceptX < this.intercepts[j][0]){
+									this.intercepts[j][0] = interceptX;
+									this.intercepts[j][1] = interceptY;
+								}
+							}
+						}
+					}else
+					if(this.angles[j] > 0.5 * Math.PI || this.angles[j] < 1.5 * Math.PI){
+						if(interceptX < this.originX){
+							var dist = interceptX - lineConst[2]; 
+							if(0 <= dist && dist < lineConst[3] - lineConst[2]){
+								if(interceptX > this.intercepts[j][0]){
+									this.intercepts[j][0] = interceptX;
+									this.intercepts[j][1] = interceptY;
+								}
+							}
+						}
+					}
+				}else{
+					var interceptX = lineConst[2];
+					var interceptY = this.gradients[j] * interceptX + this.C[j];
+					if(this.angles[j] < 0.5 * Math.PI || this.angles[j] > 1.5 * Math.PI){
+						if(interceptX > this.originX){
+							var dist = abs(interceptY - lineConst[4]) + abs(interceptY - lineConst[5]); 
+							if(dist === abs(lineConst[4] - lineConst[5])){
+								if(interceptX < this.intercepts[j][0]){
+									this.intercepts[j][0] = interceptX;
+									this.intercepts[j][1] = interceptY;
+								}
+							}
+						}
+					}else
+					if(this.angles[j] > 0.5 * Math.PI || this.angles[j] < 1.5 * Math.PI){
+						if(interceptX < this.originX){
+							var dist = abs(interceptY - lineConst[4]) + abs(interceptY - lineConst[5]); 
+							if(dist === abs(lineConst[4] - lineConst[5])){
+								if(interceptX > this.intercepts[j][0]){
+									this.intercepts[j][0] = interceptX;
+									this.intercepts[j][1] = interceptY;
+								}
+							}
+						}
+					}
+				}
+			}else{
+				if(this.angles[j] < Math.PI){
+					var interceptX = this.originX;
+					var interceptY = lineConst[0] * interceptX + lineConst[1];
+					var dist = interceptX - lineConst[2]; 
+					if(0 <= dist && dist < lineConst[3] - lineConst[2]){
+						if(this.originY < interceptY && interceptY < this.intercepts[j][1]){
+							this.intercepts[j][0] = interceptX;
+							this.intercepts[j][1] = interceptY;
+						}
+					}
+				}else
+				if(this.angles[j] > Math.PI){
+					var interceptX = this.originX;
+					var interceptY = lineConst[0] * interceptX + lineConst[1];
+					var dist = interceptX - lineConst[2]; 
+					if(0 <= dist && dist < lineConst[3] - lineConst[2]){
+						if(this.originY > interceptY && interceptY > this.intercepts[j][1]){
+							this.intercepts[j][0] = interceptX;
+							this.intercepts[j][1] = interceptY;
+						}
 					}
 				}
 			}
@@ -116,11 +200,7 @@ class sun{
 
 	cast(){
 		for(var i = 0; i < this.rayNumber; i++){
-			var a = this.originX;
-			var b = this.originY;
-			var c = this.intercepts[i][1];
-			var d = this.intercepts[i][2];
-			line(a, b, c, d);
+			line(this.originX, this.originY, this.intercepts[i][0], this.intercepts[i][1]);
 		}
 	}
 }
